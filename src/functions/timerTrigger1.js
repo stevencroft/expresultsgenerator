c0346355692c859e4f48d067e007cef3bcaaaadc
Steve C <34257347+stevencroft@users.noreply.github.com>
2024-10-04 13:16:31 +1000

    Added low, normal and high event dispatchers

--- src/functions/timerTrigger1.js
+++ src/functions/timerTrigger1.js
@@ -10,6 +10,44 @@ app.timer('timerTrigger1', {
         function getRandomInt(max) {
             return Math.floor(Math.random() * max);
         }
+
+        function getRandomIntInclusive(min, max) {
+            const minCeiled = Math.ceil(min);
+            const maxFloored = Math.floor(max);
+            return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+          }
+          
+
+        function lowEventDispatcher(event, tags)
+        //25% chance of sending an event
+        {
+            eventrandomiser = getRandomInt(12);
+            if (eventrandomiser <= 3) {
+                user.trackEvent(event, tags);
+                console.log('OPTI lowevent dispatched');
+            }
+        }     
+        
+        function normalEventDispatcher(event, tags)
+        //50% chance of sending an event
+        {
+            eventrandomiser = getRandomInt(10);
+            if (eventrandomiser > 5) {
+                user.trackEvent(event, tags);
+                console.log('OPTI normalevent dispatched');
+            }
+        }   
+        
+        function highEventDispatcher(event, tags)
+        //75% chance of sending an event
+        {
+            eventrandomiser = getRandomInt(12);
+            if (eventrandomiser <= 9) {
+                user.trackEvent(event, tags);
+                console.log('OPTI highevent dispatched');
+            }
+        }     
+
         const optimizely = optimizelySdk.createInstance({
             sdkKey: 'Ay6RMNd5gcpbVXGAonjB5',
         });
@@ -22,14 +60,19 @@ app.timer('timerTrigger1', {
 
             let hasOnFlags = false;
             //for (let i = 0; i < 1; i++) {
-            for (let i = 0; i < getRandomInt(20); i++) {
+            for (let i = 0; i < getRandomInt(50); i++) {
                 // to get rapid demo results, generate random users. Each user always sees the same variation unless you reconfigure the flag rule.
                 const userId = (Math.floor(Math.random() * (10000 - 1000) + 1000)).toString();
 
                 // Create hardcoded user & bucket user into a flag variation
                 const user = optimizely.createUserContext(userId);
 
-                // "product_sort" corresponds to a flag key in your Optimizely project
+                // Create tags
+                const tags = {
+                    revenue: getRandomIntInclusive(3000,12000)
+                  };
+
+                // Corresponds to a flag key in your Optimizely project
                 const decision = user.decide('abandoned_cart');
                 const variationKey = decision.variationKey;
                 console.log('OPTI userid: ', userId, ' assigned to variation ', decision.variationKey);
@@ -41,22 +84,34 @@ app.timer('timerTrigger1', {
 
                 if (decision.enabled) {
                     hasOnFlags = true;
-
                 }
 
-                // randomise conversion events to generate more interesting results
-                eventrandomiser = getRandomInt(3);
-                if (eventrandomiser >= 1) {
-                    user.trackEvent('purchase');
-                    console.log('OPTI event dispatched');
+                if (decision.variationKey === "control") {
+                    lowEventDispatcher('completed_purchase', tags); 
+                    lowEventDispatcher('add_to_cart'); 
+                    lowEventDispatcher('checkout_started');
                 }
-
+                if (decision.variationKey === "variation_1") {
+                    highEventDispatcher('completed_purchase', tags);
+                    highEventDispatcher('add_to_cart');
+                    highEventDispatcher('checkout_started');
+                }
+                if (decision.variationKey === "variation_2") {
+                    normalEventDispatcher('completed_purchase', tags);
+                    normalEventDispatcher('add_to_cart');
+                    normalEventDispatcher('checkout_started');
+                }
+            //     // randomise conversion events to generate more interesting results
+            //     eventrandomiser = getRandomInt(3);
+            //     if (eventrandomiser >= 1) {
+            //         user.trackEvent('purchase');
+            //         console.log('OPTI event dispatched');
+            //     }
             }
 
             if (!hasOnFlags) {
-                console.log('Flag was off for everyone');
+                console.log('OPTI Flag were off for user ', userId);
             }
         });
-        context.log('End of block');
     }
 });
